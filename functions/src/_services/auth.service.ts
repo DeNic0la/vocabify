b5caca187269e9484d5c65cebf8de194d65b5e81
Lint Action <lint-action@samuelmeuli.com>
2022-10-05 17:29:02 +0000

    Fix code style issues with Prettier

--- functions/src/_services/auth.service.ts
+++ functions/src/_services/auth.service.ts
@@ -1,16 +1,25 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
 
 export class AuthService {
-
-  async validateFirebaseIdToken(req: functions.https.Request, res: functions.Response<any>): Promise<DecodedIdToken | undefined> {
-    if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) && !(req.cookies && req.cookies.__session)) {
+  async validateFirebaseIdToken(
+    req: functions.https.Request,
+    res: functions.Response<any>
+  ): Promise<DecodedIdToken | undefined> {
+    if (
+      (!req.headers.authorization ||
+        !req.headers.authorization.startsWith('Bearer ')) &&
+      !(req.cookies && req.cookies.__session)
+    ) {
       return;
     }
 
     let idToken;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer ')
+    ) {
       idToken = req.headers.authorization.split('Bearer ')[1];
     } else if (req.cookies) {
       idToken = req.cookies.__session;
