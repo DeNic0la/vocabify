50b02ce705dfdf1bb97030c6e317ffed4e07e70b
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-10-07 00:58:51 +0200

    do the thing

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationCancel } from '@angular/router';
-import { AuthService } from '../Auth/auth.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-register',
