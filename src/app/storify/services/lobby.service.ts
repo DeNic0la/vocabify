f8473bb21651ad066ef3ce0facb4546433c044ea
Sili-Dev <85447837+Sili-Dev@users.noreply.github.com>
2022-10-07 10:08:51 -0700

    implement lobby fetching from db

--- src/app/storify/services/lobby.service.ts
+++ src/app/storify/services/lobby.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Lobby } from 'functions/src/types/lobby';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LobbyService {
 
-  constructor() { }
+  constructor(private fireStore: AngularFirestore) { }
+
+  async getLobby(id: string) {
+    const lobby = <Lobby>(await this.fireStore.collection('lobbies').doc(id).ref.get()).data();
+    if (!lobby) {
+      throw new Error('The lobby does not exist.');
+    }
+    return lobby;
+  }
+
+  async getAllLobbies() {
+    let lobbies: Lobby[] = [];
+    const firebaseLobbies = await (await this.fireStore.collection<Lobby>('lobbies').ref.get()).docs;
+    for (let lobby of firebaseLobbies) {
+      lobbies.push(lobby.data());
+    }
+    return lobbies;
+  }
 }
