15bd1bce84caf6ee55089764452b733eb8dbd939
DeNicola <nicola@fioretti.info>
2022-10-07 19:19:45 +0200

    why the actual fuck has this shit horizontal scroll fuck this

--- src/app/ui/card-grid/card-grid.component.ts
+++ src/app/ui/card-grid/card-grid.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {LobbyCardItem} from "./card-grid.types";
 
 @Component({
   selector: 'app-card-grid',
@@ -8,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class CardGridComponent implements OnInit {
   constructor() {}
 
-  public test = ['a', 'b', 'c', 'd'];
+  @Input() Cards: LobbyCardItem[] = [];
+
+  onCallback(index:number){
+    this.Cards[index].callback()
+  }
+
 
   ngOnInit(): void {}
 }
