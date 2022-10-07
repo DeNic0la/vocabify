3a4badf42a8373bd09d429eb2ed61900879a7e2d
Lint Action <lint-action@samuelmeuli.com>
2022-10-07 23:55:18 +0000

    Fix code style issues with Prettier

--- src/app/storify/storify-explore/storify-explore.component.ts
+++ src/app/storify/storify-explore/storify-explore.component.ts
@@ -3,8 +3,8 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Lobby } from '../types/lobby';
 import { LobbyItem } from './storify-explore';
 import { LobbyService } from '../services/lobby.service';
-import {Router} from "@angular/router";
-import {ButtonColor} from "../../ui/button/button.types";
+import { Router } from '@angular/router';
+import { ButtonColor } from '../../ui/button/button.types';
 
 @Component({
   selector: 'app-storify-explore',
@@ -12,39 +12,44 @@ import {ButtonColor} from "../../ui/button/button.types";
   styleUrls: ['./storify-explore.component.scss'],
 })
 export class StorifyExploreComponent implements OnInit {
+  public buttonColorDanger: ButtonColor = 'danger';
+  public buttonColorSuccess: ButtonColor = 'success';
 
-  public buttonColorDanger:ButtonColor = 'danger';
-  public buttonColorSuccess:ButtonColor = 'success'
-
-  public isLoading:boolean = true;
-  constructor(private lobbyService: LobbyService,private router:Router) {
-    lobbyService.getLobbiesToJoin().then((value:Lobby[]) => {
+  public isLoading: boolean = true;
+  constructor(private lobbyService: LobbyService, private router: Router) {
+    lobbyService.getLobbiesToJoin().then((value: Lobby[]) => {
       this.transformer(value);
     });
   }
 
-  async createLobby(){
-    if (!this.isLoading){
+  async createLobby() {
+    if (!this.isLoading) {
       this.isLoading = true;
-      this.lobbyService.createLobby().then(value => {
-        this.isLoading = false
+      this.lobbyService.createLobby().then((value) => {
+        this.isLoading = false;
         this.router.navigate(['/storify/lobby/', value]);
-      })
+      });
     }
-
   }
 
   transformer(value: Lobby[]) {
     let items: LobbyItem[] = [];
     value.forEach((i) => {
-      items.push(new LobbyItem(i.name, i.id, i.participants.length,this.getJoinLobbyCallback(i.id,this.lobbyService)));
+      items.push(
+        new LobbyItem(
+          i.name,
+          i.id,
+          i.participants.length,
+          this.getJoinLobbyCallback(i.id, this.lobbyService)
+        )
+      );
     });
     this.Lobbies = items;
     this.isLoading = false;
   }
 
-  getJoinLobbyCallback(lobbyId:string,lobbyService:LobbyService){
-    return ()=>{
+  getJoinLobbyCallback(lobbyId: string, lobbyService: LobbyService) {
+    return () => {
       lobbyService.joinLobby(lobbyId);
     };
   }
