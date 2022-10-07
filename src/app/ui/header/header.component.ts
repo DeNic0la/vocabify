2b128198a70952f837719cc500cd26cdb5af4b06
Lint Action <lint-action@samuelmeuli.com>
2022-10-07 22:53:49 +0000

    Fix code style issues with Prettier

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import firebase from 'firebase/compat/app';
 import { AuthService } from '../../auth/auth.service';
-import {HeaderService} from "../../services/header.service";
-import {Router} from "@angular/router";
-import {ToasterService} from "../../services/toaster.service";
+import { HeaderService } from '../../services/header.service';
+import { Router } from '@angular/router';
+import { ToasterService } from '../../services/toaster.service';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +14,12 @@ export class HeaderComponent {
   public isLoggedIn: boolean = false;
   public loading: boolean = false;
 
-  constructor(private authService: AuthService, public headerService: HeaderService, private router: Router, private toasterService: ToasterService) {
+  constructor(
+    private authService: AuthService,
+    public headerService: HeaderService,
+    private router: Router,
+    private toasterService: ToasterService
+  ) {
     this.loading = true;
     authService.currentUser.subscribe((user) => {
       this.isLoggedIn = !(user == undefined || false);
@@ -23,8 +28,16 @@ export class HeaderComponent {
   }
 
   public signOut() {
-    firebase.auth().signOut().then(() => this.isLoggedIn = false)
-      .then(() => this.router.navigate(['home'])
-        .then(() => this.toasterService.showToast('success', 'you have been signed out')));
+    firebase
+      .auth()
+      .signOut()
+      .then(() => (this.isLoggedIn = false))
+      .then(() =>
+        this.router
+          .navigate(['home'])
+          .then(() =>
+            this.toasterService.showToast('success', 'you have been signed out')
+          )
+      );
   }
 }
