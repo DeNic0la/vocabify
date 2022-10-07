ee5915bbb402b2d39902786043e14157333bb689
DeNicola <nicola@fioretti.info>
2022-10-08 01:53:43 +0200

    joinablle lobies

--- src/app/storify/storify-explore/storify-explore.component.ts
+++ src/app/storify/storify-explore/storify-explore.component.ts
@@ -3,6 +3,8 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Lobby } from '../types/lobby';
 import { LobbyItem } from './storify-explore';
 import { LobbyService } from '../services/lobby.service';
+import {Router} from "@angular/router";
+import {ButtonColor} from "../../ui/button/button.types";
 
 @Component({
   selector: 'app-storify-explore',
@@ -10,10 +12,26 @@ import { LobbyService } from '../services/lobby.service';
   styleUrls: ['./storify-explore.component.scss'],
 })
 export class StorifyExploreComponent implements OnInit {
-  constructor(private lobbyService: LobbyService) {
+
+  public buttonColorDanger:ButtonColor = 'danger';
+  public buttonColorSuccess:ButtonColor = 'success'
+
+  public isLoading:boolean = true;
+  constructor(private lobbyService: LobbyService,private router:Router) {
     lobbyService.getLobbiesToJoin().then((value:Lobby[]) => {
       this.transformer(value);
     });
+  }
+
+  async createLobby(){
+    if (!this.isLoading){
+      this.isLoading = true;
+      this.lobbyService.createLobby().then(value => {
+        this.isLoading = false
+        this.router.navigate(['/storify/lobby/', value]);
+      })
+    }
+
   }
 
   transformer(value: Lobby[]) {
@@ -22,6 +40,7 @@ export class StorifyExploreComponent implements OnInit {
       items.push(new LobbyItem(i.name, i.id, i.participants.length,this.getJoinLobbyCallback(i.id,this.lobbyService)));
     });
     this.Lobbies = items;
+    this.isLoading = false;
   }
 
   getJoinLobbyCallback(lobbyId:string,lobbyService:LobbyService){
