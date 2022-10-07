070bbf5e5cb91f13605cbe424af38aec1234e508
Sili-Dev <85447837+Sili-Dev@users.noreply.github.com>
2022-10-07 13:57:44 -0700

    add more functionality to backend

--- functions/src/services/lobby.service.ts
+++ functions/src/services/lobby.service.ts
@@ -1,5 +1,5 @@
 import * as admin from 'firebase-admin';
-import { Lobby } from '../types/lobby';
+import { Lobby, LobbyState } from '../types/lobby';
 import { Participant } from '../types/participant';
 import { UserService } from './user.service';
 
@@ -8,27 +8,49 @@ export class LobbyService {
   private userService = new UserService();
 
   async createLobby(uid: string): Promise<Lobby> {
-    const host = await this.userService.getUser(uid);
-    const lobby: Lobby = {
-      id: Date.now().toString(),
-      hostid: host.uid,
-      name: 'Lobby from ' + host.username,
-    };
-    await this.db.collection('lobbies').doc(lobby.id).create(lobby);
-    return lobby;
+    try {
+      const host = await this.userService.getUser(uid);
+      const lobby: Lobby = {
+        id: Date.now().toString(),
+        hostid: host.uid,
+        name: host.username + "'s Lobby",
+        story: 'Lorem Ipsum Story',
+        state: LobbyState.JOINING,
+      };
+      await this.db.collection('lobbies').doc(lobby.id).create(lobby);
+      return lobby;
+    } catch (error) {
+      throw new Error('Internal Server Error.');
+    }
   }
 
   async join(uid: string, lobbyid: string) {
     const user = await this.userService.getUser(uid);
+    const lobby = await this.getLobby(lobbyid);
+    if (lobby.state == LobbyState.IN_PROGRESS) {
+      throw new Error('The lobby is already in progress.');
+    }
     const particpant: Participant = {
       uid: user.uid,
       username: user.username,
     };
-    await this.db
-      .collection('lobbies')
-      .doc(lobbyid)
-      .collection('users')
-      .doc(user.uid)
-      .create(particpant);
+    try {
+      await this.db
+        .collection('lobbies')
+        .doc(lobbyid)
+        .collection('participants')
+        .doc(user.uid)
+        .create(particpant);
+    } catch (error) {
+      throw new Error('Internal Server error.')
+    }
+  }
+
+  private async getLobby(id: string): Promise<Lobby> {
+    const lobby = <Lobby>(await this.db.collection('lobbies').doc(id).get()).data();
+    if (!lobby) {
+      throw new Error('The lobby does not exist.');
+    }
+    return lobby;
   }
 }
