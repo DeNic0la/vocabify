ba0f080c7fd241e28537f003c482b7fecee265ac
DawidKapka <kapkadawid6@gmail.com>
2022-10-10 10:24:18 -0700

    fix toast which was incredibly complicated

--- src/app/ui/card-grid/card-grid.component.ts
+++ src/app/ui/card-grid/card-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LobbyCardItem } from './card-grid.types';
+import {ToasterService} from "../../services/toaster.service";
 
 @Component({
   selector: 'app-card-grid',
@@ -7,13 +8,14 @@ import { LobbyCardItem } from './card-grid.types';
   styleUrls: ['./card-grid.component.scss'],
 })
 export class CardGridComponent implements OnInit {
-  constructor() {}
+  constructor(private toaster: ToasterService) {}
 
   @Input() cards: LobbyCardItem[] = [];
   @Input() title: string = 'Explore';
 
   onCallback(index: number) {
-    this.cards[index].callback();
+    this.toaster.showToast('error', 'test')
+    //this.cards[index].callback();
   }
 
   ngOnInit(): void {}
