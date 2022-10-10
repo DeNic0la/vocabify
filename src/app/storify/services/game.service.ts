1fb5b446a8b2c718cd601bbbddb9cfbc840ba8b2
Sili-Dev <85447837+Sili-Dev@users.noreply.github.com>
2022-10-10 16:24:27 -0700

    fix getallrounds

--- src/app/storify/services/game.service.ts
+++ src/app/storify/services/game.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import { Functions } from '../types/functions.enum';
+import { Participant } from '../types/participant';
+import { Round } from '../types/round';
 import { HttpService } from './http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService, private fireStore: AngularFirestore,) { }
 
   public async submitAnswer(lobbyId: string, sentence: string) {
     await this.httpService.post(Functions.SUBMIT, { lobbyId, sentence });
+  }
+
+  public async getAllRounds(lobbyId: string) {
+    return this.fireStore.collection('lobbies').doc(lobbyId).collection('rounds').valueChanges();
   }
 }
