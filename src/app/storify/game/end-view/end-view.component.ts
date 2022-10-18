453674090005d8ba1ac978dc50f136d536fd9232
DawidKapka <kapkadawid6@gmail.com>
2022-10-18 11:43:17 -0700

    some improvements to end-view

--- src/app/storify/game/end-view/end-view.component.ts
+++ src/app/storify/game/end-view/end-view.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import { Lobby } from '../../types/lobby';
 import { Participant } from '../../types/participant';
+import {LobbyService} from "../../services/lobby.service";
 
 @Component({
   selector: 'app-end-view',
@@ -11,12 +12,22 @@ export class EndViewComponent implements OnInit {
   @Input('lobby') lobby: Lobby | undefined;
   @Input('story') story: string = '';
 
+  @ViewChild('summary') summarySection: ElementRef | undefined
+
   public participantsSorted: Participant[] = [];
 
-  constructor() {}
+  constructor(private lobbyService: LobbyService) {}
 
   ngOnInit(): void {
     this.participantsSorted =
       this.lobby?.participants.sort((a, b) => b.points - a.points) || [];
+  }
+
+  scrollToSummary() {
+    this.summarySection?.nativeElement.scrollIntoView({behavior: "smooth"});
+  }
+
+  public async goToHome() {
+    await this.lobbyService.leave(this.lobby?.id || '');
   }
 }
