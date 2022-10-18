c516bfce72cee4339943e0358eca9da896b61a40
Lint Action <lint-action@samuelmeuli.com>
2022-10-18 19:03:23 +0000

    Fix code style issues with Prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,13 @@ export class AppComponent {
   title = 'vocabify';
 
   constructor(public toasterService: ToasterService) {
-    const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
-    if(isAndroid) {
-      document.write('<meta name="viewport" content="width=device-width,height='+window.innerHeight+', initial-scale=1.0">');
+    const isAndroid = navigator.userAgent.toLowerCase().indexOf('android') > -1;
+    if (isAndroid) {
+      document.write(
+        '<meta name="viewport" content="width=device-width,height=' +
+          window.innerHeight +
+          ', initial-scale=1.0">'
+      );
     }
   }
 }
