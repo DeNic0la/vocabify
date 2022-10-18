1c91ea0eb50c22239452193e756d41fb1727e74c
Sili-Dev <85447837+Sili-Dev@users.noreply.github.com>
2022-10-18 14:18:54 -0700

    add winner to winnerview

--- src/app/storify/game/winner-view/winner-view.component.ts
+++ src/app/storify/game/winner-view/winner-view.component.ts
@@ -10,6 +10,7 @@ import { Lobby } from '../../types/lobby';
 import { GameService } from '../../services/game.service';
 import { LobbyState } from 'functions/src/types/lobby';
 import { AuthService } from '../../../auth/auth.service';
+import { Winner } from './winner.type';
 
 @Component({
   selector: 'app-winner-view',
@@ -25,8 +26,7 @@ export class WinnerViewComponent implements OnChanges {
 
   public isHost: boolean = false;
 
-  public winnerName: string = '';
-  public winnerStory: string = '';
+  public winners: Winner[] = [];
 
   constructor(private auth: AuthService) {
     auth.currentUser.subscribe((x) => {
@@ -41,15 +41,33 @@ export class WinnerViewComponent implements OnChanges {
       const winnerId =
         this.round.submittedStories[this.round.winner as number]?.uid || '';
       if (winnerId) {
-        this.winnerStory =
-          this.round.submittedStories.find((story) => story.uid === winnerId)
-            ?.sentence || '';
-        this.winnerName =
-          this.lobby.participants.find(
-            (participant) => participant.uid === winnerId
-          )?.username || '';
+        this.winners = [];
+        this.winners.push(this.getWinnerObj("AI's winner... (+" + this.lobby.participants.length * 50 + " Points)", winnerId));
+
+        let bestStory = this.round.submittedStories[0];
+        for (let story of this.round.submittedStories) {
+          if (bestStory.userRatings.length < story.userRatings.length) {
+            bestStory = story;
+          }
+        }
+        this.winners.push(this.getWinnerObj("The audience winner is... (+" + this.lobby.participants.length * 25 + " Points)", bestStory.uid));
       }
     }
+  }
+
+  private getWinnerObj(title: string, winnerId: string): Winner {
+    if (this.lobby && this.round) {
+      const winner: Winner = {
+        title,
+        story: this.round.submittedStories.find((story) => story.uid === winnerId)
+          ?.sentence || '',
+        username: this.lobby.participants.find(
+          (participant) => participant.uid === winnerId
+        )?.username || '',
+      }
+      return winner;
+    }
+    return { story: '', title: '', username: '' }
   }
 
   async continue() {
